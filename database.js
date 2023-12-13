3e97953ca1aa1a99ff55c0ba7593049a08c316fe
Yash Tulsani <yash.t20@iiits.in>
2023-12-13 22:46:41 +0530

    fixed issue in database.js file

--- database.js
+++ database.js
@@ -1,3 +1,5 @@
+const mongoose=require('mongoose');
+
 class Database {
     #connectionString=`mongodb+srv://phoenixReborn:${process.env.DB_PASSWORD}@cluster0.gywglgt.mongodb.net/?retryWrites=true&w=majority`
   constructor() {
