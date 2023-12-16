ba0f2c008896596cbefa2771680170888357b7fa
Tejas <tejasparse1@gmail.com>
2023-12-16 16:38:12 +0530

    Chatbot done

---
+++ controllers/chatControllers.js
@@ -0,0 +1,44 @@
+const dialogflow = require("@google-cloud/dialogflow");
+const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
+const PROJECTID = CREDENTIALS.project_id;
+const CONFIGURATION = {
+    credentials: {
+        private_key: CREDENTIALS["private_key"],
+        client_email: CREDENTIALS["client_email"]
+    }
+}
+const sessionClient = new dialogflow.SessionsClient(CONFIGURATION);
+
+const detectIntent = async (languageCode, queryText, sessionid) => {
+    let sessionPath = sessionClient.projectAgentSessionPath(PROJECTID, sessionid);
+
+    let request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: queryText,
+                languageCode: languageCode
+            }
+        } 
+    };
+
+    const responses = await sessionClient.detectIntent(request);
+    console.log(responses);
+    const result = responses[0].queryResult;
+    console.log(result);
+    return {
+        replyText: result.fulfillmentText
+    }
+}
+
+exports.getMessageReply = async (req,res)=>{
+
+    console.log(req.body);
+
+    const reply = await detectIntent("en", req.body.message, "abcd123");
+    // const reply = {
+    //     replyText: "This is a default response please try to activate Dialogflow chatbot"
+    // }
+
+    res.json(reply)
+}
--- server.js
+++ server.js
@@ -1,15 +1,23 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const db=require('./database')
-const router=require('./routes')
+const router=require('./routes');
+const cors = require('cors')
+
+
 
 const PORT=process.env.PORT || 5000;
 
+app.use(cors())
 app.use(express.static('public'));
 app.use(express.json());
+
 app.use(router);
 
-
+app.get("*", (req,res)=>{
+    res.send("Error 404")
+})
 app.listen(5000, () => console.log('Server running on port 5000'));
 
 
