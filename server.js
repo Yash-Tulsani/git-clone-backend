b8d020ce5128ec8a1f0437e9ceb9ec84ce7c6e4d
Yash Tulsani <yash.t20@iiits.in>
2023-12-19 18:03:00 +0530

    Revert "added cors config"
    This reverts commit dc6e70b0a328307579e3edea8fe913babc032eb2.
    
    :wq

--- server.js
+++ server.js
@@ -10,14 +10,7 @@ GenerateData.GenerateData()
 
 const PORT=process.env.PORT || 5000;
 
-const cors = require("cors");
-
-const corsOptions ={
-    origin: ["http://localhost:3000","https://git-clone-frontend.vercel.app/"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-}
-
-app.use(cors(corsOptions));
+app.use(cors())
 app.use(express.static('public'));
 app.use(express.json());
 
@@ -46,11 +39,11 @@ app.use((err,req,res,next) => {
     })
 })
 
-// app.get("*", (req,res)=>{
-//     res.status(404).json({
-//         message: "Route not found",
-//         status: 404
-//     })
-// })
+app.get("*", (req,res)=>{
+    res.status(404).json({
+        message: "Route not found",
+        status: 404
+    })
+})
 
 module.exports = app;
