790c16f34231d0babde58194a57d672ec089c343
abhay-zel <abhay.pratap@zel.finance>
2023-12-20 08:47:13 +0530

    signout backend

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {signup, signin, google} = require('../controllers/authController');
+const {signup, signin, google, signout} = require('../controllers/authController');
 
 router.post('/signup', signup);
 router.post('/signin', signin);
 router.post('/google', google);
+router.get('/signout', signout);
 
 module.exports = router;
